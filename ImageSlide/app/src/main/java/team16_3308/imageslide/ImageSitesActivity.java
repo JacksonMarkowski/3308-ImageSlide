617cee9d204dfd5cf9c5b287e535a89de9a66125
JacksonMarkowski <markowski.jackson@gmail.com>
2015-12-03 15:46:37 -0700

    Switched ImageSites and Settings

--- ImageSlide/app/src/main/java/team16_3308/imageslide/ImageSitesActivity.java
+++ ImageSlide/app/src/main/java/team16_3308/imageslide/ImageSitesActivity.java
@@ -1,8 +1,14 @@
 package team16_3308.imageslide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**Activity that is used to select where to get images from for the application. */
 public class ImageSitesActivity extends AppCompatActivity {
@@ -14,14 +20,44 @@ public class ImageSitesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_sites);
+        setContentView(R.layout.activity_image_sites); DBHandler db = new DBHandler(this);
+
+        db.open();
+        try{
+        }catch(Exception e){}
+
+        ArrayList<String> subList = new ArrayList<String>();
+
+        subList = db.readAll();
+        //Populate list code from http://stackoverflow.com/questions/5070830/populating-a-listview-using-arraylist
+        ListView lv = (ListView) findViewById(R.id.subredditList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                subList
+        );
+
+        lv.setAdapter(arrayAdapter);
+
     }
 
-    /** Finishes/closes the activity and returns to the previous activity.
+    public void sendSub(View view){
+        Intent intent = new Intent(this, DBHandler.class);
+        EditText editText = (EditText) findViewById(R.id.subreddit);
+        String message = editText.getText().toString();
+        DBHandler db = new DBHandler(this);
+        db.open();
+        db.insertSub(message);
+        Intent refresh = new Intent(this, ImageSitesActivity.class);
+        startActivity(refresh);
+    }
+
+    /** Finishes/closes the image sites activity and returns to the previous activity.
      *
      * @param view Reference to the backButton.
      */
     public void backButtonClick(View view) {
         finish();
     }
+
 }
--- ImageSlide/app/src/main/java/team16_3308/imageslide/SettingsActivity.java
+++ ImageSlide/app/src/main/java/team16_3308/imageslide/SettingsActivity.java
@@ -22,37 +22,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
-
-        DBHandler db = new DBHandler(this);
-        db.open();
-        try{
-        }catch(Exception e){}
-
-        ArrayList<String> subList = new ArrayList<String>();
-
-        subList = db.readAll();
-        //Populate list code from http://stackoverflow.com/questions/5070830/populating-a-listview-using-arraylist
-        ListView lv = (ListView) findViewById(R.id.subredditList);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                subList
-        );
-
-        lv.setAdapter(arrayAdapter);
-
-    }
-
-    public void sendSub(View view){
-        Intent intent = new Intent(this, DBHandler.class);
-        EditText editText = (EditText) findViewById(R.id.subreddit);
-        String message = editText.getText().toString();
-        DBHandler db = new DBHandler(this);
-        db.open();
-        db.insertSub(message);
-        Intent refresh = new Intent(this, SettingsActivity.class);
-        startActivity(refresh);
     }
 
     /** Finishes/closes the settings activity and returns to the previous activity.
